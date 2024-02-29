493dee5f89b4f4a0af257ec85db7c06bca1f1b3a
Timur Azim <papaas73@gmail.com>
2024-02-29 19:33:05 +0600

    Переделал код

--- Skillbox 15.4/Skillbox 15.4.cpp
+++ Skillbox 15.4/Skillbox 15.4.cpp
@@ -2,17 +2,22 @@
 
 void printNumbers(int N, bool isEven) {
     std::cout << "Numbers from 0 to " << N << ": ";
-    for (int i = 0; i <= N; i++) {
-        std::cout << i + isEven << " ";
-        i++;
+    for (int i = 0; i <= N; i += 2) {
+        if (!isEven) {
+            if (i == 0) continue; 
+            std::cout << i << " ";
+        }
+        else {
+            std::cout << i << " ";
+        }
     }
     std::cout << std::endl;
 }
 
 int main() {
-    const int N = 10;
+    const int N = 10; 
 
-    std::cout << "Even numbers " << N << ": ";
+    std::cout << "Even numbers" << N << ": ";
     for (int i = 0; i <= N; i += 2) {
         std::cout << i << " ";
     }
@@ -22,5 +27,10 @@ int main() {
     for (int i = 1; i <= N; i += 2) {
         std::cout << i << " ";
     }
+    std::cout << std::endl;
+
+    printNumbers(N, true);
+    printNumbers(N, false);
+
     return 0;
 }
